3254af2011bec965bd648ee4787c6428d8a1e67f
BiggusMaximus <annastyabagas164@gmail.com>
2022-11-02 00:00:22 +0700

    testing dor display

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <Arduino.h>
+#include "display.h"
+
 void setup()
 {
   Serial.begin(115200);
+  start_display();
 }
 
 void loop()
 {
-  Serial.print("Test");
+  display.clearDisplay();
+  display.setTextColor(BLACK, WHITE);
+  display.setCursor(0, 0);
+  display.print("TEST ");
+  Serial.println("Test");
+  display.display();
 }
